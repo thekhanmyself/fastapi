e7c6d6c1b4ca5a9440bacd45445a24b3bbf8d8f8
thekhanmyself <thekhanmyself@gmail.com>
2022-06-17 10:42:46 +0600

    final test

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,8 @@ from app.oauth2 import create_access_token
 from app import models
 
 
-SQLALCHEMY_DATABASE_URL =  'postgresql://postgres:S.18112006.k@localhost:5432/test'
-#SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+#SQLALCHEMY_DATABASE_URL =  'postgresql://postgres:S.18112006.k@localhost:5432/test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 engine= create_engine(SQLALCHEMY_DATABASE_URL)
 
